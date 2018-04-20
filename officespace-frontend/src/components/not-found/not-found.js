620991f561424bbead4dce130d15d8d5c3a1a3ec
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-04-19 22:19:35 -0600

    404 status page

--- officespace-frontend/src/components/not-found/not-found.js
+++ officespace-frontend/src/components/not-found/not-found.js
@@ -12,7 +12,7 @@ function NotFound() {
 			<p className="lead" style={{fontFamily:"nunito", fontWeight:"bold", color:"#ffffff"}}>This is embarrassing...</p>
 			<a className="btn btn-default" href='/' style={{border: '1px solid black', marginTop:"60px", marginLeft:"-10px",color: "#ffffff", width: "17%", textAlign: "text-center"}}>Go back to your home...</a>
 			<div className="img">
-					<img src={require ('./img/dog.gif')} style={{marginTop:"-95px", marginLeft:"-700px"}}/>
+					<img src={require ('../img/dog.gif')} style={{marginTop:"-95px", marginLeft:"-700px"}}/>
 </div>
 		</div>
 
---
+++ officespace-frontend/src/pages/login-page.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import "./login-page.css";
+
+
+  
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+
+  
+
+
+export default class LoginPage extends Component {
+     constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  
+
+
+
+  
+  render() {
+    return (
+
+	 // <div className = "col-sm-8"> 
+  //       <div className = "row"> 
+
+    	<div className="login-card">
+
+<div className="brand animated fadeIn">
+    	    	<img src="./img/Untitled-2.png" alt="logo" id="logo" />
+    	    	 <div className="paragraph">Please login to your account</div>
+    	  
+    	    
+
+    	<div className = "row"> 
+        <div className="col-sm-12">
+ <Form>
+
+
+        <div className="form-group">
+         
+          <Input type="email" className="form-control" id="inputTextEmail" placeholder="Email" />
+        </div>
+    	<div className = "row"> 
+        <div className="col-sm-12">
+       <div className="form-group">
+       
+          <Input type="password" className="form-control" id="inputTextPassword" placeholder="Password" />
+        </div>
+          <button className="btn btn">Login</button>
+          <p>Forgot Password</p>
+          	</div>
+     	</div>
+        </Form>
+   </div>
+        </div>
+     	</div>
+     	</div>
+// </div>
+// </div>
+
+
+  )
+
+}
+}
