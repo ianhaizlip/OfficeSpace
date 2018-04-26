0dca7e68b260227bbedc496cd15b55a86a350396
jdloude <jdloude@gmail.com>
2018-04-25 20:39:44 -0600

    combined some files into folders

--- officespace-backend/src/config.js
+++ officespace-backend/src/config.js
@@ -1,13 +1,13 @@
 import {accessKeyId,secretAccessKey} from './s3-config.json'
-export const smtp = {
-    host: 'smtp.sendgrid.net',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: 'apikey', // generated ethereal user
-        pass: 'SendGridApiKey'  // generated ethereal password
-    }
-};
+// export const smtp = {
+//     host: 'smtp.sendgrid.net',
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//         user: 'apikey', // generated ethereal user
+//         pass: 'SendGridApiKey'  // generated ethereal password
+//     }
+// };
 
 
 
